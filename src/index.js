0d136c22d1530ff7ba436f2204b41a3052ca3a81
Atulit Anand <64276998+icecoffee-dev@users.noreply.github.com>
2021-06-28 01:02:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// import React from "react";
-import ReactDOM from "react-dom";
-import { StrictMode } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import App from './App';
-
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.querySelector("#root")
-);
-
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./Components/Counter";
@@ -22,4 +7,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
->>>>>>> 1de39c9 (updated boiler plate , some other fixes and removed react router)
